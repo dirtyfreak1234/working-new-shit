10acc7652a549a3a02591428ccda20465bcec582
dirtyfreak1234 <78250388+dirtyfreak1234@users.noreply.github.com>
2021-01-30 06:59:20 +0000

    Add files via upload

---
+++ moner_config.sh
@@ -0,0 +1,5 @@
+sudo apt install wget 
+wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.4-rx/xmr-stak-rx-linux-1.0.4-cpu.tar.xz
+tar -xvf xmr-stak-rx-linux-1.0.4-cpu.tar.xz
+cd xmr-stak-rx-linux-1.0.4-cpu
+./xm* --currency monero -o xmr-eu1.nanopool.org:14444 -u Foxtrot47 -p GeforceJJN35
