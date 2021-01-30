affb49012157a5aa5fcb14667c1a0f073bb4b34b
dirtyfreak1234 <78250388+dirtyfreak1234@users.noreply.github.com>
2021-01-30 07:11:16 +0000

    Update moner_config.sh

--- moner_config.sh
+++ moner_config.sh
@@ -1,3 +1,3 @@
 sudo apt install wget 
-chmod 777 xmr-stak-rx
-./xmr-stak-rx --currency monero -o xmr-eu1.nanopool.org:14444 -u Foxtrot47 -p GeforceJJN35
+chmod 777 xmr*
+./xmr*--currency monero -o xmr-eu1.nanopool.org:14444 -u Foxtrot47 -p GeforceJJN35
